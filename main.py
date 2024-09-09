52cda3e6dc370474538b7c20aea44aa750d0f7df
Sweta Sergeeva <sweta.sergeeva@gmail.com>
2024-09-09 13:06:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+def on_button_click():
+    label_out.config(text=f'Привет, {str(entry.get())}')
+
+root  = tk.Tk()
+root.title("В - Вежливость")
+root.geometry('400x150')
+
+label = tk.Label(root, text="Как вас зовут?")
+label.pack()
+
+entry = tk.Entry(root)
+entry.pack()
+
+button = tk.Button(root, text="Ответить", command=on_button_click)
+button.pack()
+
+label_out = tk.Label(root, text="")
+label_out.pack()
+
+
+root.mainloop()
